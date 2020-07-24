d0fc8ee0e9a141f5372cb97e1b1bdfe3761076cb
lfotev96 <lfotev96@gmail.com>
2020-07-24 15:22:44 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/app.dart';
 
 import 'login_page.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: RecipesApp(),
     );
   }
 }
